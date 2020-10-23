6df7607f2b2ca02b4670fb2482c7ec433f9f2332
hectordcies <72934221+hectordcies@users.noreply.github.com>
2020-10-23 18:56:31 +0200

    v1.0

--- ejercicios tema 4/Aleatorio.java
+++ ejercicios tema 4/Aleatorio.java
@@ -2,7 +2,7 @@ import java.util.Random;
 public class Aleatorio {
     public static void main(String args []){
         Random r = new Random();
-        int alea = r.nextInt(1000) + 1 ;
+        int alea = r.nextInt(10000) + 1 ;
         System.out.println(alea);
     }
     
